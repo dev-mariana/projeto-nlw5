45b128054d43e21878982e17a0975551466ce745
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-04-21 10:33:23 -0300

    refactoring SettingsController and add users controller, service and entitie

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { SettingsController } from './controllers/SettingsController';
+import { UsersController } from './controllers/UsersController';
 import { SettingsRepository } from './repositories/SettingsRepository';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 routes.post('/settings', settingsController.create);
+routes.post('/users', usersController.create);
 
 export { routes };
