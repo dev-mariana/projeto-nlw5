9eee9e03c8ca726e9219bf222495e42012b54233
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-04-22 22:48:16 -0300

    add socket.io

---
+++ src/database/migrations/1619142474950-CreateConnections.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateConnections1619142474950 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
