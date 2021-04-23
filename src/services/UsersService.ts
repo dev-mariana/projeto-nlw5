69e79ae164109c1c34af449fbd7af78b5b72fb28
Mariana Bastos <maribastossrodrigues@gmail.com>
2021-04-23 10:05:55 -0300

    add connections entitie, repository and service

--- src/services/UsersService.ts
+++ src/services/UsersService.ts
@@ -22,6 +22,14 @@ class UsersService {
 
         return user;
     };
+
+    async findByEmail(email: string) {
+        const user = await this.usersRepository.findOne({
+          email,
+        });
+        
+        return user;
+    }
 }
 
 export { UsersService };
